4b11629f53887e0686b3821caa1249381732f5ff
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-01-22 16:58:53 +0300

    test account functionality

--- user-test.py
+++ user-test.py
@@ -1,6 +1,7 @@
 from cgi import test
 from lib2to3.pytree import Leaf
 from re import A
+from socket import if_nametoindex
 from termios import B0
 from user import User
 import unittest
@@ -33,4 +34,27 @@ class TestUser(unittest.TestCase):
         test_user = User('Neema', 'Zora', 'mamdaku', 'hideme')
         test_user.save_user()
         self.new_user.delete_user()
-        self.assertEqual(Leaf(User.user_User) 2)
+        self.assertEqual(Leaf(User.user_User), 2)
+
+    def test_get_user_by_username(self):
+        self.new_user.save_user()
+        test_user = User('Neema', 'Zora', 'mamdaku', 'hideme')
+        test_user.save_user()
+        found_user = User.find_by_user_name('mamdaku')
+        self.assertEqual(found_user.user_name, test_user.user_name)
+
+    def test_user_present(self):
+        self.new_user.save_user()
+        test_user = User('Neema', 'Zora', 'mamdaku', 'hideme')
+        test_user.save_user()
+        user_present = User.user_present('mamdaku')
+        self.assertTrue(user_present)
+
+    def test_show_user(self):
+        self.assertEqual(User.show_user(),
+                         User.user_User)
+
+
+if __name__ == '__main__':
+ unittest.main()
+
