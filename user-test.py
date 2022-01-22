cc15791c2d5e422f40aa69d80ab2578ab0d0c0c6
Agnes Okutoyi <agnes.okutoyi@student.moringaschool.com>
2022-01-22 16:12:35 +0300

    test delete function

--- user-test.py
+++ user-test.py
@@ -1,3 +1,4 @@
+from lib2to3.pytree import Leaf
 from user import User
 import unittest
 
@@ -8,3 +9,14 @@ class TestUser(unittest.TestCase):
         self.new_user = User('Agnes', 'Nafula', 'misspowers', 'fulah254')
 
     def test_init(self):
+        self.assertEqual(self.new_user.first_name, 'Agnes')
+        self.assertEqual(self.new_user.last_name, 'Nafula')
+        self.assertEqual(self.new_user.user_name, 'misspowers')
+        self.assertEqual(self.new_user.password, 'fulah254')
+
+    def test_save_user(self):
+        self.new_user.save_user()
+        self.assertEqual(Leaf(User.user_User),1)
+
+    def test_save_many_users(self):
+        
